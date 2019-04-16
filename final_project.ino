89d82fb041ebc3f6638ba8722946846e4c2389a4
Lauren Ho <lwyho68@gmail.com>
2019-04-16 17:29:10 -0400

    both overflow and timer compare versions

--- final_project.ino
+++ final_project.ino
@@ -1,69 +1,57 @@
-/*
-  Button
-
-  Turns on and off a light emitting diode(LED) connected to digital pin 13,
-  when pressing a pushbutton attached to pin 2.
-
-  The circuit:
-  - LED attached from pin 13 to ground
-  - pushbutton attached to pin 2 from +5V
-  - 10K resistor attached to pin 2 from ground
-
-  - Note: on most Arduinos there is already an LED on the board
-    attached to pin 13.
-
-  created 2005
-  by DojoDave <http://www.0j0.org>
-  modified 30 Aug 2011
-  by Tom Igoe
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/Button
-*/
-
-// constants won't change. They're used here to set pin numbers:
-//const int buttonPin = A0;     // the number of the pushbutton pin
-const int buttonPins[8] = {2, 3, 4, 5, 6, 7, 8, 11}; //8 pins, (~9, ~10 still available)
-const int playButton = A0;
-const int clearButton = A1;
-const int ledPin =  12;      // the number of the LED pin
-
-// variables will change:
-int buttonState = 0;         // variable for reading the pushbutton status
+// Timer compare version
+const int ledPin = 8;
+volatile byte state = LOW;
 
 void setup() {
-  Serial.begin(9600);
-
-  // Initialize the pins
   pinMode(ledPin, OUTPUT);
-  for (int i = 0; i < 8; i++) {
-    pinMode(buttonPins[i], INPUT);
-  }
-
-  // Set up the timer config
-  // TODO: note that we will want to change OCR1A value to interrupt faster than 1 second
-  bitSet(TCCR1B, WGM12);        // set WGM1[2] = 1
-  bitSet(TIMSK1, OCH1E1A);      // enable interrupt on OCR1A register
-  OCR1A = 15624;                // set Output Compare Register value
-  TCCR1B = TCCR1B | 00000101;   // set CS1[2:0] = 101
+  // enable interrupts
+  interrupts();
+  Serial.begin(19200);
+  bitSet(TCCR2B, WGM22);        // set WGM22[2] = 1
+  bitSet(TIMSK2, OCIE2A);       // enable interrupt on OCR2A register
+  OCR2A = 255;                  // set Output Compare Register value
+  TCCR2B |= (1 << CS22) | (0 << CS21) | (1 << CS20) ; // set the prescaler to 1024
 }
 
 void loop() {
-  // read the state of the pushbutton value:
-  buttonState = digitalRead(buttonPin);
-  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
-  if (buttonState == HIGH) {
-    // turn LED on:
-    digitalWrite(ledPin, HIGH);
-    Serial.println("Pressed");
-  } else {
-    // turn LED off:
-    digitalWrite(ledPin, LOW);
-    Serial.println("Default low");
-  }
+  Serial.println(state);
+  digitalWrite(ledPin, state);
+  delay(100);
 }
 
-ISR (TIER1_COMPA_vect) {
-  digitalWrite(ledPin, !digitalRead(ledPin));
+ISR(TIMER2_COMPA_vect) {
+  state = !state;
 }
+
+////Working signal processing
+//int ledPin = 8;
+//volatile byte state = LOW;
+//
+//void setup() {
+//  pinMode(ledPin, OUTPUT);
+//
+//  // enable interrupts
+//  interrupts();
+//
+//  Serial.begin(19200);
+//
+//  //Timer 2 setup
+//  TCCR2A =   (1 << COM2A1) | (1 << COM2A0)   //COM2A: controls OC2A/Arduino pin 11 output mode
+//             | (1 << COM2B1) | (1 << COM2B0) //COM2B: controls OC2B/Arduino pin 3 output mode
+//             | (0 << WGM21) | (1 << WGM20);  //WGM: Phase correct PWM (doubles period)
+//  TCCR2B = (0 << WGM22)
+//           | (1 << CS22) | (0 << CS21) | (1 << CS20) ;  //Timer2:prescaler=1024
+//  TIMSK2 = (1 << TOIE2);                                //Enable Timer2 Overflow Interrupt
+//  TIFR2  = (1 << TOV2);                                 //Clear TOV2 / clear pending interrupts
+//
+//}
+//
+//void loop() {
+//  Serial.println(state);
+//  digitalWrite(ledPin, state);
+//}
+//// interrupt every 512 cycles (16,000,000/512Hz=31250)
+//ISR (TIMER2_OVF_vect)
+//{
+//  state = !state;
+//}
